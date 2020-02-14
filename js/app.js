6e1ade2e236a4de897a93e0ca25c25401440038a
prabhuvashwin <ashwinvprabhu@icloud.com>
2020-02-13 16:05:21 -0800

    Update layout changes
    Update layout changes

--- js/app.js
+++ js/app.js
@@ -1,10 +1,27 @@
 let archiveId;
+let sessionId;
+let role;
+let name = 'Unnamed';
+let publisher;
 
 const handleError = error => {
   if ( error ) {
     console.error( error );
   }
 };
+
+const onSubmit = () => {
+  const roleInput = document.querySelector( '#role' );
+  role = roleInput.value;
+  name = document.querySelector( '#name' ).value || 'Unnamed';
+
+  const login = document.querySelector( '.login-container' );
+  login.classList.add( 'hide' );
+  const home = document.querySelector( '.home-container' );
+  home.classList.remove( 'hide' );
+  // Run init()
+  init();
+}
 
 const setupTextChat = session => {
   // =============== Setup text chat ===============
@@ -17,7 +34,7 @@ const setupTextChat = session => {
 
     session && session.signal( {
       type: 'msg',
-      data: msgTxt.value
+      data: `>>>${name}: ${msgTxt.value}`
     }, error => {
       if ( error ) {
         console.error( `Error sending signal: ${error.name}, ${error.message}` );
@@ -32,9 +49,9 @@ const setupTextChat = session => {
 const init = () => {
 
   // Set initial states for archiving buttons
-  document.querySelector( '#start' ).classList.remove( 'hide' );
-  document.querySelector( '#view' ).classList.add( 'hide' );
-  document.querySelector( '#stop' ).classList.add( 'hide' );
+  document.querySelector( '#start' ).disabled = false;
+  document.querySelector( '#view' ).disabled = true;
+  document.querySelector( '#stop' ).disabled = true;
   archiveId = null;
 
   // Get apiKey, sessionId, token info and initialize session
@@ -42,9 +59,11 @@ const init = () => {
     fetch( `${SAMPLE_SERVER_BASE_URL}/session` )
       .then( res => res.json() )
       .then( json => {
-        const { apiKey, sessionId, token } = json;
+        const { apiKey, sessionId: _sessionId, token } = json;
 
-        if ( !apiKey || !sessionId && !token ) return;
+        if ( !apiKey || !_sessionId && !token ) return;
+
+        sessionId = _sessionId;
 
         const session = OT.initSession( apiKey, sessionId );
 
@@ -63,6 +82,31 @@ const init = () => {
 const setupFeatures = ( args = {} ) => {
   const { session, token } = args;
 
+  const layoutContainer = document.getElementById( 'layout' );
+
+  const options = {
+    maxRatio: 3/2,             // The narrowest ratio that will be used (default 2x3)
+    minRatio: 9/16,            // The widest ratio that will be used (default 16x9)
+    fixedRatio: false,         // If this is true then the aspect ratio of the video is maintained and minRatio and maxRatio are ignored (default false)
+    // alignItems: 'start',      // Can be 'start', 'center' or 'end'. Determines where to place items when on a row or column that is not full
+    // bigClass: "OT_big",        // The class to add to elements that should be sized bigger
+    // bigPercentage: 0.8,        // The maximum percentage of space the big ones should take up
+    // bigFixedRatio: false,      // fixedRatio for the big ones
+    // bigAlignItems: 'center',   // How to align the big items
+    // smallAlignItems: 'center', // How to align the small row or column of items if there is a big one
+    bigMaxRatio: 3/2,          // The narrowest ratio to use for the big elements (default 2x3)
+    bigMinRatio: 9/16,         // The widest ratio to use for the big elements (default 16x9)
+    bigFirst: true,            // Whether to place the big one in the top left (true) or bottom right
+    animate: true,             // Whether you want to animate the transitions
+    animateDuration: 200,
+    animateEasing: "swing",
+    window: window,            // Lets you pass in your own window object which should be the same window that the element is in
+    // ignoreClass: 'OT_ignore',  // Elements with this class will be ignored and not positioned. This lets you do things like picture-in-picture
+  };
+
+  // Initialize the layout container and get a reference to the layout method
+  const { layout } = initLayoutContainer( layoutContainer, options );
+
   // Subscribe to a newly created stream
   session.on( 'streamCreated', event => {
     const subscriberOptions = {
@@ -71,52 +115,72 @@ const setupFeatures = ( args = {} ) => {
       height: '100%'
     };
     session.subscribe( event.stream, 'subscriber', subscriberOptions, handleError );
+    layout();
   } );
 
   // Start the archive
   session.on( 'archiveStarted', event => {
     archiveId = event.id;
     console.log( `Archive started: ${archiveId}` );
-    document.querySelector( '#stop' ).classList.remove( 'hide' );
-    document.querySelector( '#start' ).classList.add( 'hide' );
+    document.querySelector( '#stop' ).disabled = false;
+    document.querySelector( '#start' ).disabled = true;
+    document.querySelector( '#view' ).disabled = true;
   } );
 
   // Stop the archive
   session.on( 'archiveStopped', event => {
     archiveId = event.id;
     console.log( `Archive stopped: ${archiveId}` );
-    document.querySelector( '#start' ).classList.add( 'hide' );
-    document.querySelector( '#stop' ).classList.add( 'hide' );
-    document.querySelector( '#view' ).classList.remove( 'hide' );
+    document.querySelector( '#start' ).disabled = false;
+    document.querySelector( '#stop' ).disabled = true;
+    document.querySelector( '#view' ).disabled = false;
   } );
 
   session.on( 'sessionDisconnected', event => {
     console.log( `You were disconnected from the session: ${event.reason}` );
   } );
 
-  // initialize the publisher
-  const publisherOptions = {
-    insertMode: 'append',
-    width: '100%',
-    height: '100%',
-  };
-  const publisher = OT.initPublisher( 'publisher', publisherOptions, handleError );
-
-  // Connect to the session
-  session.connect( token, error => {
-    if ( error ) {
-      handleError( error );
-    } else {
-      // If the connection is successful, publish the publisher to the session
-      session.publish( publisher, handleError );
-    }
-  } );
+  if ( role !== 'Viewer' ) {
+    // initialize the publisher
+    const publisherOptions = {
+      insertMode: 'append',
+      width: '100%',
+      height: '100%',
+      mirror: true,
+      name,
+    };
+    const p = document.querySelector( '#publisher' );
+    p.classList.remove( 'hide' );
+    publisher = OT.initPublisher( 'publisher', publisherOptions, handleError );
+    window.__publisher = publisher;
+    // Connect to the session
+    session.connect( token, error => {
+      if ( error ) {
+        handleError( error );
+      } else {
+        // If the connection is successful, publish the publisher to the session
+        session.publish( publisher, handleError );
+        layout();
+      }
+    } );
+  }
+  else {
+    // Connect to the session
+    session.connect( token, error => {
+      if ( error ) {
+        handleError( error );
+      } else {
+        // If the connection is successful, publish the publisher to the session
+        layout();
+      }
+    } );
+  }
 
   // Receive a message and append it to the history
   const msgHistory = document.querySelector( '#history' );
   session.on( 'signal:msg', event => {
     const msg = document.createElement( 'p' );
-    msg.textContent = event.data;
+    msg.textContent = `${event.data}`;
     msg.className = event.from.connectionId === session.connection.connectionId ? 'mine' : 'theirs';
     msgHistory.appendChild( msg );
     msg.scrollIntoView();
@@ -124,7 +188,7 @@ const setupFeatures = ( args = {} ) => {
 }
 
 // Start recording
-const startArchive = () => { // eslint-disable-line no-unused-vars
+const startArchive = () => {
   fetch( `${SAMPLE_SERVER_BASE_URL}/archive/start`, {
     method: 'POST',
     mode: 'cors',
@@ -144,32 +208,92 @@ const startArchive = () => { // eslint-disable-line no-unused-vars
     console.log( `Error calling startArchive - message: ${err}` );
   } );
 
-  document.querySelector( '#start' ).classList.add( 'hide' );
-  document.querySelector( '#stop' ).classList.remove( 'hide' );
+  document.querySelector( '#start' ).disabled = true;
+  document.querySelector( '#stop' ).disabled = false;
+  document.querySelector( '#view' ).disabled = true;
 }
 
 // Stop recording
-const stopArchive = () => { // eslint-disable-line no-unused-vars
-  fetch( `${SAMPLE_SERVER_BASE_URL}/archive/${archiveId}/stop` )
-    .then( response => {
-      console.log( `stopArchive() complete - response: ${response}` );
-      return response.json();
-    } ).then( data => {
-      console.log( `Successfully stopped archive - data returned: ${data}` );
-    } ).catch( err => {
-      console.log( `Error calling stopArchive - message: ${err}` );
-    } );
-  document.querySelector( '#stop' ).classList.add( 'hide' );
+const stopArchive = () => {
+  fetch( `${SAMPLE_SERVER_BASE_URL}/archive/${archiveId}/stop`, {
+    method: 'POST',
+    mode: 'cors',
+    cache: 'no-cache',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer',
+    body: JSON.stringify( {} ),
+  } ).then( response => {
+    console.log( `stopArchive() complete - response: ${response}` );
+    return response.json();
+  } ).then( data => {
+    console.log( `Successfully stopped archive - data returned: ${data}` );
+  } ).catch( err => {
+    console.log( `Error calling stopArchive - message: ${err}` );
+  } );
+  document.querySelector( '#stop' ).disabled = true;
   document.querySelector( '#view' ).disabled = false;
-  document.querySelector( '#stop' ).classList.remove( 'hide' );
+  document.querySelector( '#start' ).disabled = false;
 }
 
 // Get the archive status. If it is  "available", download it. Otherwise, keep checking
 // every 5 secs until it is "available"
-const viewArchive = () => { // eslint-disable-line no-unused-vars
+const viewArchive = () => {
   document.querySelector( '#view' ).disabled = true;
   window.location = `${SAMPLE_SERVER_BASE_URL}/archive/${archiveId}/view`;
 }
 
-// Run init()
-init();
+const startBroadcast = () => {
+  stopArchive();
+  document.querySelector( '#view' ).disabled = true;
+
+  document.querySelector( '#bStart' ).disabled = true;
+  document.querySelector( '#bStop' ).disabled = false;
+}
+
+const stopBroadcast = () => {
+  document.querySelector( '#bStart' ).disabled = false;
+  document.querySelector( '#bStop' ).disabled = true;
+}
+
+const showPublisher = () => {
+  document.querySelector( '#pShow' ).disabled = true;
+  document.querySelector( '#pHide' ).disabled = false;
+  document.querySelector( '#publisher' ).classList.remove( 'hide' );
+}
+
+const hidePublisher = () => {
+  document.querySelector( '#pShow' ).disabled = false;
+  document.querySelector( '#pHide' ).disabled = true;
+  document.querySelector( '#publisher' ).classList.add( 'hide' );
+}
+
+const startPublishingVideo = () => {
+  document.querySelector( '#startVideo' ).disabled = true;
+  document.querySelector( '#stopVideo' ).disabled = false;
+
+  publisher.publishVideo( true );
+}
+
+const stopPublishingVideo = () => {
+  document.querySelector( '#startVideo' ).disabled = false;
+  document.querySelector( '#stopVideo' ).disabled = true;
+
+  publisher.publishVideo( false );
+}
+
+const startPublishingAudio = () => {
+  document.querySelector( '#startAudio' ).disabled = true;
+  document.querySelector( '#stopAudio' ).disabled = false;
+
+  publisher.publishAudio( true );
+}
+
+const stopPublishingAudio = () => {
+  document.querySelector( '#startAudio' ).disabled = false;
+  document.querySelector( '#stopAudio' ).disabled = true;
+
+  publisher.publishAudio( false );
+}
